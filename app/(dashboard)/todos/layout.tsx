453257dd2a02d454c4bc1c287bd686e92c501fcd
Taciano <taciano3003@gmail.com>
2023-09-22 22:41:22 -0300

    server actions lesson completed.

---
+++ app/(dashboard)/todos/layout.tsx
@@ -0,0 +1,15 @@
+import NewTodoForm from "@/components/NewTodoForm";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div>
+        <NewTodoForm />
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
