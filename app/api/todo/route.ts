5c1f15a15e6c5306db90b85aee4e72d1adcd1375
Taciano <taciano3003@gmail.com>
2023-09-24 13:51:52 -0300

    api routes and server action mutation lessons complete.

---
+++ app/api/todo/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export const GET = async (request: Request) => {
+  return NextResponse.json({ message: "hello" });
+};
+
+export const POST = async (request: Request) => {
+  const data = await request.json();
+  return NextResponse.json({ message: "hello" });
+};
